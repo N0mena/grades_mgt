26afb9c2ce13716dfbd891dcd8671272a9e4924c
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-14 11:54:18 +0300

    feat: add AuthController

---
+++ src/main/java/hei/school/minou/endpoint/rest/controller/controller/AuthController.java
@@ -0,0 +1,19 @@
+package hei.school.minou.endpoint.rest.controller.controller;
+
+import hei.school.minou.endpoint.rest.controller.dto.LoginRequest;
+import hei.school.minou.endpoint.rest.controller.dto.LoginResponse;
+import hei.school.minou.service.auth.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/auth/login")
+  public LoginResponse login(@RequestBody LoginRequest request) {
+    return authService.login(request);
+  }
+}
