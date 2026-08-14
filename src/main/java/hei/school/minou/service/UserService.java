3ba71758b5ee4a8aac1fe0efa6a2deea0c4ecd8a
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-14 11:50:18 +0300

    fix: saveUser builder fixed

--- src/main/java/hei/school/minou/service/UserService.java
+++ src/main/java/hei/school/minou/service/UserService.java
@@ -9,6 +9,7 @@ import hei.school.minou.repository.model.JUser;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,9 +18,19 @@ public class UserService {
 
   private final UserRepository userRepository;
   private final UserMapper userMapper;
+  private final PasswordEncoder passwordEncoder;
 
   public User saveUser(User user) {
-    return userMapper.toDomain(userRepository.save(userMapper.toJpa(user)));
+    User encoded =
+        User.builder()
+            .id(user.id())
+            .firstName(user.firstName())
+            .lastName(user.lastName())
+            .role(user.role())
+            .email(user.email())
+            .password(passwordEncoder.encode(user.password()))
+            .build();
+    return userMapper.toDomain(userRepository.save(userMapper.toJpa(encoded)));
   }
 
   public User getUserById(UUID id) {
