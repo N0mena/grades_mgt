c24dd1f185c9ff2da5a2b2783778ccc53f9e4c6e
MiotyRenala <hei.mioty.2@gmail.com>
2026-08-15 21:07:33 +0300

    Feat : Add formattage and fix test

--- src/main/java/hei/school/minou/endpoint/rest/controller/controller/AuthController.java
+++ src/main/java/hei/school/minou/endpoint/rest/controller/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-public class  AuthController {
+public class AuthController {
 
   private final AuthService authService;
 
--- src/main/java/hei/school/minou/endpoint/rest/controller/controller/LoginViewController.java
+++ src/main/java/hei/school/minou/endpoint/rest/controller/controller/LoginViewController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginViewController {
 
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login";
-    }
+  @GetMapping("/login")
+  public String showLoginPage() {
+    return "login";
+  }
 }
