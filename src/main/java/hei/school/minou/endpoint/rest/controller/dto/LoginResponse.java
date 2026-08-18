c15a0ab79611aa4a94384b3a00e8884593abfda8
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-18 12:49:16 +0300

    feat: add success , failure and token methods and attributs

--- src/main/java/hei/school/minou/endpoint/rest/controller/dto/LoginResponse.java
+++ src/main/java/hei/school/minou/endpoint/rest/controller/dto/LoginResponse.java
@@ -5,15 +5,15 @@ import lombok.Builder;
 
 @Builder
 public record LoginResponse(boolean success, String token, String errorMessage, User user) {
-    public static LoginResponse success(String token) {
-        return new LoginResponse(true, token, null, null);
-    }
+  public static LoginResponse success(String token) {
+    return new LoginResponse(true, token, null, null);
+  }
 
-    public static LoginResponse success(String token, User user) {
-        return new LoginResponse(true, token, null, user);
-    }
+  public static LoginResponse success(String token, User user) {
+    return new LoginResponse(true, token, null, user);
+  }
 
-    public static LoginResponse failure(String errorMessage) {
-        return new LoginResponse(false, null, errorMessage, null);
-    }
+  public static LoginResponse failure(String errorMessage) {
+    return new LoginResponse(false, null, errorMessage, null);
+  }
 }
